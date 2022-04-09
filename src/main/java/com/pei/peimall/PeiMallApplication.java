6c52877e65de8fe2f0f3ea5ab744e96bd1a2e722
linpeitong <peggy5885251@gmail.com>
2022-04-09 23:23:17 +0800

    初始化 Spring Boot project

---
+++ src/main/java/com/pei/peimall/PeiMallApplication.java
@@ -0,0 +1,13 @@
+package com.pei.peimall;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PeiMallApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PeiMallApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/pei/peimall/PeiMallApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pei.peimall;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PeiMallApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
