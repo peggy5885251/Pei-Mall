21a5ad0f5228fa67ae71cf89279015034d525c22
peitung <peggy5885251@gmail.com>
2022-04-19 23:56:38 +0800

    商品功能 - 實作修改商品

--- src/main/java/com/pei/peimall/controller/ProductController.java
+++ src/main/java/com/pei/peimall/controller/ProductController.java
@@ -50,4 +50,10 @@ public class ProductController {
         Product updateProduct = productService.getProductById(productId);
         return ResponseEntity.status(HttpStatus.OK).body(updateProduct);
     }
+
+    @DeleteMapping("/products/{productId}")
+    public ResponseEntity<?> deleteProductById(@PathVariable Integer productId){
+        productService.deleteProductById(productId);
+        return ResponseEntity.status(HttpStatus.NO_CONTENT).build();
+    }
 }
