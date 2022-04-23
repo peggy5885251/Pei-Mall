205e92e77cf35722f8a40d53079658d77597df27
peitung <peggy5885251@gmail.com>
2022-04-24 01:33:22 +0800

    商品功能 - 實作查詢商品列表(分頁)

--- src/main/java/com/pei/peimall/dao/Impl/ProductDaoImpl.java
+++ src/main/java/com/pei/peimall/dao/Impl/ProductDaoImpl.java
@@ -31,6 +31,7 @@ public class ProductDaoImpl implements ProductDao {
                 "FROM product WHERE 1 = 1  ";
 
         Map<String, Object> map = new HashMap<>();
+        // 查詢條件
         if (productQueryParams.getCategory()!=null ){
             sql += " AND category = :category ";
             map.put("category", productQueryParams.getCategory().name());
@@ -39,8 +40,13 @@ public class ProductDaoImpl implements ProductDao {
             sql += " AND product_name LIKE :search ";
             map.put("search", "%"+productQueryParams.getSearch()+"%");
         }
-
+        // 排序
         sql += " ORDER BY " + productQueryParams.getOrderBy()+ " " +productQueryParams.getSort();
+        // 分頁
+        sql += " LIMIT :limit OFFSET :offset ";
+        map.put("limit", productQueryParams.getLimit());
+        map.put("offset", productQueryParams.getOffset());
+
         List<Product> productList = namedParameterJdbcTemplate.query(sql, map, new ProductRowMapper());
         return productList;
     }
